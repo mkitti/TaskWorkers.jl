895062260c30875b24b562f2d4fc1d79b7c33792
Mark Kittisopikul <kittisopikulm@janelia.hhmi.org>
2020-10-09 02:31:47 -0400

    Use taskrun instead of run. Major restructuring for v0.2

--- README.md
+++ README.md
@@ -1,3 +1,97 @@
 # TaskWorkers.jl
 
-Create a loop that will execute closures or functions sent to it.
+TaskWorkers.jl creates a loop that executes functions or closures sent to it.
+The main use is ensuring those functions will execute on the `Task` that
+the `TaskWorker` was started on. For example, this could be used to
+ensure that functions are executed on Julia's root `Task`.
+
+The main purpose of this package is to facilitate interoperation of
+Julia with other software that may be confused with Julia's
+`Tasks` which do not copy the stack by default.
+
+## Installation
+
+This package is currently not registered in the general Julia registry
+as significant prototyping is underway. To add from the Julia REPL:
+
+```
+]add https://github.com/mkitti/TaskWorkers.jl#master
+```
+
+## Quick Usage and Demo
+
+```julia
+julia> Base.current_task() == Base.roottask
+true
+
+julia> using TaskWorkers
+
+julia> TaskWorkers.startworker_and_repl()
+               _
+   _       _ _(_)_     |  Documentation: https://docs.julialang.org
+  (_)     | (_) (_)    |
+   _ _   _| |_  __ _   |  Type "?" for help, "]?" for Pkg help.
+  | | | | | | |/ _` |  |
+  | | |_| | | | (_| |  |  Version 1.5.0 (2020-08-01)
+ _/ |\__'_|_|_|\__'_|  |  Official https://julialang.org/ release
+|__/                   |
+
+julia> Base.current_task() == Base.roottask
+false
+
+julia> taskrun() do
+           Base.current_task() == Base.roottask
+       end
+true
+
+julia>
+```
+
+## Extended Usage
+
+Because of Julia's stack conventions for `Task`s it may be necessary to ensure certain commands run on a certain `Task`, in particular the root `Task` (`Base.roottask`).
+
+The following will start a new REPL on a new `Task` and start running a `TaskWorker` on the root `Task`:
+
+```julia
+using TaskWorkers
+worker = TaskWorker()
+@async(Base.run_main_repl(true,true,true,true,true)); sleep(1); start(worker)
+```
+
+A shortcut for the above is available as `TaskWorkers.startworker_and_repl()`.
+Replace `Base.run_main_repl` with any function which will continue on a new `Task`.
+The `TaskWorker` will take over the current `Task` with its execution loop.
+
+From the newly created REPL you can then check to see which commands are running on the root `Task`
+
+```julia
+julia> Base.current_task() == Base.roottask
+false
+
+julia> taskrun(worker) do
+           Base.current_task() == Base.roottask
+       end
+true
+```
+
+If worker is not specified (e.g. `taskrun() do ... end`), `TaskWorkers.worker[]` will be used.
+`TaskWorkers.worker` is a `Ref{TaskWorker}` can be populated manually or with these convenience functions:
+
+* `TaskWorkers.startworker()`
+* `TaskWorkers.startworker_and_repl()`
+
+This is only meant as a convenience. Otherwise, it is recommended that you track your own global references
+to `TaskWorker`s.
+
+## Applications
+
+This package was developed to help Julia interoperate with other software that depend 
+In particular, this could be useful for use with [JavaCall.jl](https://github.com/JuliaInterop/JavaCall.jl).
+
+## Related Julia issues, pull requests, and packages
+
+* https://github.com/JuliaLang/julia/pull/31983 (https://github.com/JuliaLang/julia/pull/31983)
+* https://github.com/JuliaLang/julia/pull/35726 (Create a Base.root_task method and document it)
+* https://github.com/JuliaLang/julia/pull/35048 (Allow the the REPL backend to run on the root Task; Merged as of Julia 1.5)
+** Julia pre-1.5, consider https://github.com/mkitti/RootTaskREPL.jl
--- src/TaskWorkers.jl
+++ src/TaskWorkers.jl
@@ -1,9 +1,27 @@
+"""
+    TaskWorkers
+
+    Creates worker loops that runs functions on a specific `Task`
+
+    See TaskWorker, taskrun, TaskWorkers.startworker_and_repl
+"""
 module TaskWorkers
 
-import Base: put!, take!, run
+import Base: put!, take!
 
-export TaskWorker, start, stop, run
+export TaskWorker, start, stop, taskrun
 
+
+"""
+    TaskWorker()
+
+    Create a new TaskWorker
+
+    # Usage
+
+    worker = TaskWorker()
+    @async(doSomethingOnAnotherTask()); start(worker)
+"""
 mutable struct TaskWorker
     running::Bool
     recv::Channel
@@ -12,27 +30,65 @@ mutable struct TaskWorker
     TaskWorker() = new(false,Channel(1),Channel(1))
 end
 
+# Private, do not use directly
 put!(w::TaskWorker,t) = put!(w.recv,t)
 take!(w::TaskWorker) = take!(w.resp)
 
+"""
+    worker::Ref{TaskWorker}
+
+    Default `TaskWorker` reference when a `TaskWorker` is not specified.
+
+    See also startworker, startworker_and_repl
+"""
+const worker = Ref{TaskWorker}()
+
+"""
+    startworker()
+
+    Create a new TaskWorker (TaskWorkers.worker[]) and start it
+"""
 function startworker()
-    global worker = TaskWorker()
-    start(worker)
+    sleep(1)
+    worker[] = TaskWorker()
+    start(worker[])
 end
 
+"""
+    startworker_and_repl(interactive=true, quiet=true, banner=true, history_file=true, color_set=true, args...)
+
+    Starts a new Julia REPL on a new `Task` via `@async` and runs a new `TaskWorker` on the current `Task`
+    See Base.run_main_repl in client.jl for argument definitions
+"""
+function startworker_and_repl(interactive=true, quiet=true, banner=true, history_file=true, color_set=true, args...)
+    @async Base.run_main_repl(interactive, quiet, banner, history_file, color_set, args...)
+    startworker()
+end
+
+"""
+    start(w::TaskWorker)
+
+    Start the worker loop for a `TaskWorker`
+"""
 function start(w::TaskWorker)
     w.running = true
     w.task = current_task()
     worker_loop(w)
 end
 
+"""
+    stop(w::TaskWorker)
+
+    Stop the worker loop for a `TaskWorker`
+"""
 function stop(w::TaskWorker)
     if w.running
         w.running = false
-        run(w,()->nothing)
+        taskrun(w,()->nothing)
     end
 end
 
+# Private implementation
 function worker_loop(w::TaskWorker)
     while(w.running)
         func_and_args = take!(w.recv)
@@ -55,10 +111,27 @@ function worker_loop(w::TaskWorker)
     nothing
 end
 
-run(w::TaskWorker, f::Function, args... ; kwargs...) =
-    length(kwargs) > 0 ? run(w,((f,args...),kwargs)) : run(w,(f,args...))
+"""
+    taskrun(w::TaskWorker, f::Function, args...)
+    taskrun(f::Function, w::TaskWorker, args...)
+    taskrun(w::TaskWorker) do ... end
+    taskrun(f::Function, args...)
+    taskrun(args...) do ... end
 
-function run(w::TaskWorker,t::Tuple)
+    Run a function f on a worker w
+    If a worker is not provided as in the last two forms,
+    taskrun will use TaskWorkers.worker[]
+"""
+taskrun(w::TaskWorker, f::Function, args... ; kwargs...) =
+    length(kwargs) > 0 ? taskrun(w,((f,args...),kwargs)) : taskrun(w,(f,args...))
+taskrun(f::Function, w::TaskWorker, args... ; kwargs...) =
+    length(kwargs) > 0 ? taskrun(w,((f,args...),kwargs)) : taskrun(w,(f,args...))
+taskrun(f::Function, args...; kwargs...) =
+    length(kwargs) > 0 ? taskrun(worker[],((f,args...),kwargs)) : taskrun(worker[],(f,args...))
+
+# Private interface, could change
+# t could be either (f,args...) or ((f,args...),kwargs)
+function taskrun(w::TaskWorker,t::Tuple)
     put!(w,t)
     out = take!(w)
     if isa(out,Exception)
